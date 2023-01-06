f5e2e3d6055c398d9832b3d739558a3268ccbe55
adbean <adbeanx@outlook.com>
2023-01-06 18:22:45 +0800

    feat: migrate to Montserrat

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,7 @@ const config = {
         },
         theme: {
           customCss: [
+            require.resolve("./src/css/fonts.css"),
             require.resolve("./src/css/custom.css"),
             require.resolve("./src/css/buttons.css"),
           ],
