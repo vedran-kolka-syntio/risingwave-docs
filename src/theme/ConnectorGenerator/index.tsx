2da300f540e1e5c698275057478bee5508ebc918
Ad-Bean <adbeanx@outlook.com>
2023-03-22 19:19:07 +0800

    fix: dark mode color

--- src/theme/ConnectorGenerator/index.tsx
+++ src/theme/ConnectorGenerator/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, Fragment, useEffect, useMemo } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import FormControl from "@mui/material/FormControl";
@@ -14,6 +15,7 @@ import CodeBlock from "@theme/CodeBlock";
 import Stack from "@mui/material/Stack";
 import LoadingButton from "@mui/lab/LoadingButton";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import { useColorMode } from "@docusaurus/theme-common";
 
 import sleep from "../../utils/sleep";
 import styles from "./styles.module.css";
@@ -63,102 +65,119 @@ export const ConnectorGenerator = ({}: Props) => {
     setConnectoer("Kafka");
   }, [connectorType]);
 
+  const { colorMode } = useColorMode();
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: colorMode,
+        },
+      }),
+    [colorMode]
+  );
+
   return (
-    <Box className={styles.mainContainer}>
-      <FormControl className={styles.formContainer}>
-        <FormLabel
-          id="demo-controlled-radio-buttons-group"
-          className={styles.labelText}
-        >
-          Connector type:
-        </FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-controlled-radio-buttons-group"
-          name="controlled-radio-buttons-group"
-          value={connectorType}
-          onChange={handleTypeChange}
-        >
-          <FormControlLabel
-            value="Source"
-            control={<Radio />}
-            label="Source"
-            className={styles.radioLabel}
-          />
-          <FormControlLabel
-            value="Sink"
-            control={<Radio />}
-            label="Sink"
-            className={styles.radioLabel}
-          />
-        </RadioGroup>
-      </FormControl>
-      <Divider light sx={{ marginTop: "1rem" }} />
-      <Typography variant="h5" gutterBottom className={styles.header}>
-        {connectorType === "Source" ? "Create source" : "Create sink"}
-      </Typography>
-
-      <FormControl variant="standard" sx={{ mt: 1.5, minWidth: 350 }} fullWidth>
-        <Stack sx={{ width: "50%" }} mb={1}>
-          <InputLabel id="demo-simple-select-standard-label">
-            Select connector
-          </InputLabel>
-          <Select
-            fullWidth
-            className={styles.selectConnector}
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-standard"
-            value={connector}
-            onChange={handleConnectorChange}
-            label="Connector"
+    <ThemeProvider theme={theme}>
+      <Box className={styles.mainContainer}>
+        <FormControl className={styles.formContainer}>
+          <FormLabel
+            id="demo-controlled-radio-buttons-group"
+            className={styles.labelText}
           >
-            {connectorType === "Source"
-              ? Connectors?.map((conn, idx) => (
-                  <MenuItem value={conn} key={idx}>
-                    {conn}
-                  </MenuItem>
-                ))
-              : Sinks?.map((conn, idx) => (
-                  <MenuItem value={conn} key={idx}>
-                    {conn}
-                  </MenuItem>
-                ))}
-          </Select>
-        </Stack>
-
-        {mapToSchema.get(`${connectorType}-${connector}`) && (
-          <div className="jsonForms">
-            <JsonForms
-              key={`${connectorType}-${connector}`}
-              schema={mapToSchema.get(`${connectorType}-${connector}`)}
-              uischema={mapToUISchema.get(`${connectorType}-${connector}`)}
-              data={data}
-              renderers={materialRenderers}
-              cells={materialCells}
-              onChange={({ data, errors }) => setData(data)}
+            Connector type:
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={connectorType}
+            onChange={handleTypeChange}
+          >
+            <FormControlLabel
+              value="Source"
+              control={<Radio />}
+              label="Source"
+              className={styles.radioLabel}
             />
-          </div>
-        )}
-      </FormControl>
-      <Stack flexDirection="row" justifyContent="end" my={2}>
-        <LoadingButton
-          loading={loading}
-          loadingPosition="start"
-          startIcon={<RocketLaunchIcon />}
-          variant="contained"
-          onClick={() => {
-            setLoading(true);
-            sleep(1000).then(() => {
-              setLoading(false);
-            });
-          }}
+            <FormControlLabel
+              value="Sink"
+              control={<Radio />}
+              label="Sink"
+              className={styles.radioLabel}
+            />
+          </RadioGroup>
+        </FormControl>
+        <Divider light sx={{ marginTop: "1rem" }} />
+        <Typography variant="h5" gutterBottom className={styles.header}>
+          {connectorType === "Source" ? "Create source" : "Create sink"}
+        </Typography>
+
+        <FormControl
+          variant="standard"
+          sx={{ mt: 1.5, minWidth: 350 }}
+          fullWidth
         >
-          Generate SQL statement
-        </LoadingButton>
-      </Stack>
-      <CodeBlock language="sql">
-        {`CREATE ${connectorType.toUpperCase()} IF NOT EXISTS ${
-          data?.sourceName || `${connectorType.toLowerCase()}_name`
-        } 
+          <Stack sx={{ width: "50%" }} mb={1}>
+            <InputLabel id="demo-simple-select-standard-label">
+              Select connector
+            </InputLabel>
+            <Select
+              fullWidth
+              className={styles.selectConnector}
+              labelId="demo-simple-select-standard-label"
+              id="demo-simple-select-standard"
+              value={connector}
+              onChange={handleConnectorChange}
+              label="Connector"
+            >
+              {connectorType === "Source"
+                ? Connectors?.map((conn, idx) => (
+                    <MenuItem value={conn} key={idx}>
+                      {conn}
+                    </MenuItem>
+                  ))
+                : Sinks?.map((conn, idx) => (
+                    <MenuItem value={conn} key={idx}>
+                      {conn}
+                    </MenuItem>
+                  ))}
+            </Select>
+          </Stack>
+
+          {mapToSchema.get(`${connectorType}-${connector}`) && (
+            <div className="jsonForms">
+              <JsonForms
+                key={`${connectorType}-${connector}`}
+                schema={mapToSchema.get(`${connectorType}-${connector}`)}
+                uischema={mapToUISchema.get(`${connectorType}-${connector}`)}
+                data={data}
+                renderers={materialRenderers}
+                cells={materialCells}
+                onChange={({ data, errors }) => setData(data)}
+              />
+            </div>
+          )}
+        </FormControl>
+        <Stack flexDirection="row" justifyContent="end" my={2}>
+          <LoadingButton
+            loading={loading}
+            loadingPosition="start"
+            startIcon={<RocketLaunchIcon />}
+            variant="contained"
+            onClick={() => {
+              setLoading(true);
+              sleep(1000).then(() => {
+                setLoading(false);
+              });
+            }}
+          >
+            Generate SQL statement
+          </LoadingButton>
+        </Stack>
+        <CodeBlock language="sql">
+          {`CREATE ${connectorType.toUpperCase()} IF NOT EXISTS ${
+            data?.sourceName || `${connectorType.toLowerCase()}_name`
+          } 
 WITH (
   connector='${connector.toLowerCase()}',  
   topic='${data?.topic || "demo_topic"}', 
@@ -172,12 +191,13 @@ WITH (
   properties.group.id='demo_consumer_name' 
 ) 
 ROW FORMAT ${data?.rowFormat || "JSON"} ${
-          data?.message ? "MESAAGE '" + data?.message + "'" : ""
-        }
+            data?.message ? "MESAAGE '" + data?.message + "'" : ""
+          }
 ROW SCHEMA LOCATION CONFLUENT
 SCHEMA REGISTRY 'http://127.0.0.1:8081';`}
-      </CodeBlock>
-    </Box>
+        </CodeBlock>
+      </Box>
+    </ThemeProvider>
   );
 };
 
